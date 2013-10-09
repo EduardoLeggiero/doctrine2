6ebe1ddf8dec64ec3a9b2b24a216a3d0fcd6df09
Eduardo <leggieroeduardo@gmail.com>
2013-10-09 22:46:56 +0200

    [DDC-2661] Fix bug in YamlDriver not passing options from id to mapField()

--- lib/Doctrine/ORM/Mapping/Driver/YamlDriver.php
+++ lib/Doctrine/ORM/Mapping/Driver/YamlDriver.php
@@ -263,6 +263,10 @@ class YamlDriver extends FileDriver
                     $mapping['columnDefinition'] = $idElement['columnDefinition'];
                 }
 
+                if (isset($idElement['options'])) {
+                    $mapping['options'] = $idElement['options'];
+                }
+
                 $metadata->mapField($mapping);
 
                 if (isset($idElement['generator'])) {
